891d2dcd6b7ca23576a080aa2454922d8c8b2d8f
XSeann <seannejmalimpenet@gmail.com>
2023-05-19 14:53:34 -0700

    hhh

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,9 @@ app.use('/api/user', userRoutes)
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => {
+    //app.listen(process.env.PORT, () => {
       console.log('connected to db & listening on port', process.env.PORT)
-    })
+    //})
   })
   .catch((error) => {
     console.log(error)
